7ac962344516e1d890d9bb5c4c7bf4c8f9f9b874
BetterZhang <betterzhang.dev@gmail.com>
2017-07-13 10:42:56 +0800

    Practice 10~11 commit

--- app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice10HistogramView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice10HistogramView.java
@@ -2,11 +2,16 @@ package com.hencoder.hencoderpracticedraw1.practice;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
 
 public class Practice10HistogramView extends View {
+
+    private Paint mPaint = new Paint();
 
     public Practice10HistogramView(Context context) {
         super(context);
@@ -26,5 +31,46 @@ public class Practice10HistogramView extends View {
 
 //        综合练习
 //        练习内容：使用各种 Canvas.drawXXX() 方法画直方图
+        mPaint.setAntiAlias(true);
+
+        // 画图表坐标轴
+        Path path = new Path();
+        path.moveTo(150, 80);
+        path.lineTo(150, 500);
+        path.lineTo(900, 500);
+        mPaint.setColor(Color.WHITE);
+        mPaint.setStyle(Paint.Style.STROKE);
+        mPaint.setStrokeWidth(2);
+        canvas.drawPath(path, mPaint);
+
+        // 画矩形
+        Path path2 = new Path();
+        path2.addRect(170, 490, 250, 500, Path.Direction.CW);
+        path2.addRect(270, 430, 350, 500, Path.Direction.CW);
+        path2.addRect(370, 430, 450, 500, Path.Direction.CW);
+        path2.addRect(470, 380, 550, 500, Path.Direction.CW);
+        path2.addRect(570, 350, 650, 500, Path.Direction.CW);
+        path2.addRect(670, 300, 750, 500, Path.Direction.CW);
+        path2.addRect(770, 400, 850, 500, Path.Direction.CW);
+        mPaint.setStyle(Paint.Style.FILL);
+        mPaint.setColor(Color.parseColor("#72B916"));
+        canvas.drawPath(path2, mPaint);
+
+        // 画文字
+        mPaint.setColor(Color.WHITE);
+        mPaint.setTextSize(25);
+        canvas.drawText("Froyo", 170, 530, mPaint);
+        canvas.drawText("GB", 290, 530, mPaint);
+        canvas.drawText("ICS", 390, 530, mPaint);
+        canvas.drawText("JB", 490, 530, mPaint);
+        canvas.drawText("KitKat", 580, 530, mPaint);
+        canvas.drawText("L", 700, 530, mPaint);
+        canvas.drawText("M", 800, 530, mPaint);
+
+        // 画图表文字
+        mPaint.setTextSize(40);
+        canvas.drawText("直方图", 450, 650, mPaint);
+
+        mPaint.reset();
     }
 }
--- app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice11PieChartView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice11PieChartView.java
@@ -2,11 +2,16 @@ package com.hencoder.hencoderpracticedraw1.practice;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
 
 public class Practice11PieChartView extends View {
+
+    private Paint mPaint = new Paint();
 
     public Practice11PieChartView(Context context) {
         super(context);
@@ -26,5 +31,50 @@ public class Practice11PieChartView extends View {
 
 //        综合练习
 //        练习内容：使用各种 Canvas.drawXXX() 方法画饼图
+        mPaint.setAntiAlias(true);
+
+        // 画6个扇形
+        mPaint.setColor(Color.parseColor("#FFC107"));
+        canvas.drawArc(200, 100, 700, 600, -60, 59, true, mPaint);
+        mPaint.setColor(Color.parseColor("#9C27B0"));
+        canvas.drawArc(200, 103, 700, 603, 0, 6, true, mPaint);
+        mPaint.setColor(Color.parseColor("#9E9E9E"));
+        canvas.drawArc(200, 106, 700, 606, 8, 6, true, mPaint);
+        mPaint.setColor(Color.parseColor("#009688"));
+        canvas.drawArc(200, 109, 700, 609, 16, 45, true, mPaint);
+        mPaint.setColor(Color.parseColor("#2196F3"));
+        canvas.drawArc(197, 109, 697, 609, 63, 117, true, mPaint);
+        mPaint.setColor(Color.parseColor("#F44336"));
+        canvas.drawArc(190, 95, 690, 595, -180, 120, true, mPaint);
+        mPaint.setColor(Color.WHITE);
+        mPaint.setTextSize(40);
+        canvas.drawText("饼图", 400, 700, mPaint);
+
+        // 画文字
+        Path path1 = new Path();
+        path1.moveTo(680, 250);
+        path1.rLineTo(100, 0);
+        mPaint.setColor(Color.WHITE);
+        mPaint.setStyle(Paint.Style.STROKE);
+        mPaint.setStrokeWidth(2);
+        canvas.drawPath(path1, mPaint);
+        mPaint.reset();
+        mPaint.setTextSize(25);
+        mPaint.setColor(Color.WHITE);
+        canvas.drawText("Marshmallow", 800, 250, mPaint);
+
+        Path path2 = new Path();
+        path2.moveTo(650, 500);
+        path2.rLineTo(100, 0);
+        mPaint.setColor(Color.WHITE);
+        mPaint.setStyle(Paint.Style.STROKE);
+        mPaint.setStrokeWidth(2);
+        canvas.drawPath(path2, mPaint);
+        mPaint.reset();
+        mPaint.setTextSize(25);
+        mPaint.setColor(Color.WHITE);
+        canvas.drawText("Jelly Bean", 800, 500, mPaint);
+
+        mPaint.reset();
     }
 }
